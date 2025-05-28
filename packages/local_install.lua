39af2ecb8bba9a244ce44dbe7615068a074e6a2f
Iota <enoua5@gmail.com>
2025-05-27 20:15:57 -0600

    Lua local install

---
+++ packages/local_install.lua
@@ -0,0 +1,10 @@
+
+local args = shell.parse(...)
+local program = args[1]
+if not string.gmatch(program, "^[%d%l%u%-_]$") then
+    print("invalid package name")
+    return
+end
+os.execute("rm -rf /tmp/" .. program)
+os.execute("cp -r /packages/" .. program .. "/install /tmp/" .. program)
+os.execute("install --from=/tmp --fromDir=/" .. program)
