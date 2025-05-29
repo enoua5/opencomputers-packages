8560ed91ce004bab8554525740bff1a8dbae6bfa
Iota <enoua5@gmail.com>
2025-05-28 19:36:06 -0600

    debugging

--- dist/lttp/lib/lttp.lua
+++ dist/lttp/lib/lttp.lua
@@ -6,40 +6,73 @@ local serialization = require("serialization")
 local uuid = require("uuid")
 local tcp_listeners = {}
 local function getPort(args)
+    print(
+        "getPort",
+        table.unpack(args)
+    )
     if args[1] == "connection" or args[1] == "close" then
+        print("getPort => args[3] =", args[4])
         return args[4]
     elseif args[1] == "message" then
+        print("getPort => args[4] =", args[5])
         return args[5]
     end
+    print("getPort => -1; unknown type", args[0])
     return -1
 end
 function ____exports.listen(port, timeout)
     if timeout == nil then
         timeout = 5
     end
+    print("listen", port, timeout)
     if tcp_listeners[port] ~= nil then
         error("Port already bound", 0)
     end
     local message_timeouts = {}
     tcp_listeners[port] = function(name, ...)
         local args = {...}
+        print(
+            "tcp_listener[",
+            port,
+            "]",
+            name,
+            ...
+        )
         if getPort(args) == port then
+            print("port match")
             local action, channel = table.unpack(args)
+            print("action", action, "channel", channel)
             if action == "connection" then
+                print("handle connection")
                 local timeout_id = event.timer(
                     timeout,
-                    function() return network.tcp.close(channel) end
+                    function()
+                        print("timed out, closing", channel)
+                        network.tcp.close(channel)
+                    end
                 )
                 message_timeouts[channel] = timeout_id
             elseif action == "close" then
+                print("handle close")
                 if message_timeouts[channel] ~= nil then
+                    print("timeout still active, cancelling")
                     event.cancel(message_timeouts[channel])
                     message_timeouts[channel] = nil
                 end
             elseif action == "message" then
+                print("handle message")
                 local document = args[3]
                 local address = args[4]
+                print("document", document)
+                print("address", address)
                 local function respond(status, headers, body)
+                    print(
+                        "respond",
+                        channel,
+                        status,
+                        headers,
+                        body
+                    )
                     network.tcp.send(
                         channel,
                         serialization.serialize({status = status, headers = headers, body = body})
@@ -49,16 +82,22 @@ function ____exports.listen(port, timeout)
                 local body = nil
                 do
                     local function ____catch()
+                        print("invalid")
                         respond(422)
                         return true
                     end
                     local ____try, ____hasReturned, ____returnValue = pcall(function()
+                        print("trying parse")
                         local document_data = serialization.unserialize(document)
+                        print("document_data", document_data)
                         if type(document_data) == "table" then
+                            print("document is object")
                             if type(document_data.headers) == "table" then
+                                print("document headers")
                                 headers = document_data.headers
                             end
                             if type(document_data.body) == "string" then
+                                print("document body")
                                 body = document_data.body
                             end
                         end
@@ -70,6 +109,7 @@ function ____exports.listen(port, timeout)
                         return ____returnValue
                     end
                 end
+                print("push lttp_request")
                 computer.pushSignal(
                     "lttp_request",
                     channel,
@@ -82,12 +122,16 @@ function ____exports.listen(port, timeout)
             end
         end
     end
+    print("adding listener to port", port)
     event.listen("tcp", tcp_listeners[port])
     network.tcp.listen(port)
 end
 function ____exports.unlisten(port)
+    print("ending listener on port", port)
     if tcp_listeners[port] ~= nil then
+        print("removing event listener")
         event.ignore("tcp", tcp_listeners[port])
+        tcp_listeners[port] = nil
     end
     network.tcp.unlisten(port)
 end
@@ -98,66 +142,124 @@ function ____exports.request(address, port, headers, body, connection_timeout, r
     if response_timeout == nil then
         response_timeout = 5
     end
+    print(
+        "request",
+        address,
+        port,
+        headers,
+        body,
+        connection_timeout,
+        response_timeout
+    )
     local request_id = uuid.next()
+    print("request_id", request_id)
     local channel = nil
     local channel_open = false
     local response = nil
     local function handler(name, ...)
         local args = {...}
+        print("client handler", name, ...)
         local action, actual_channel = table.unpack(args)
+        print(
+            "action",
+            action,
+            "actual_channel",
+            actual_channel,
+            "channel",
+            channel
+        )
         if channel == actual_channel then
+            print("channel match")
             if action == "connection" then
+                print("handle connection")
                 channel_open = true
+                print("push", "connect" .. request_id)
                 computer.pushSignal("connect" .. request_id)
             end
             if action == "close" then
+                print("handle close")
                 channel_open = false
+                print("push", "connect" .. request_id)
                 computer.pushSignal("connect" .. request_id)
+                print("push", "message" .. request_id)
                 computer.pushSignal("message" .. request_id)
             end
             if action == "message" then
+                print("handle message")
                 response = args[3]
+                print("response", response)
+                print("push", "message" .. request_id)
                 computer.pushSignal("message" .. request_id)
             end
         end
     end
+    print("listen to tcp events")
     event.listen("tcp", handler)
     channel = network.tcp.open(address, port)
+    print("tcp connection requested on channel", channel)
+    print("waiting on", "connect" .. request_id)
     event.pull(connection_timeout, "connect" .. request_id)
+    print("wait over", "channel_open", channel_open)
     if not channel_open then
+        print("channel never opened")
         event.ignore("tcp", handler)
         error("connection timeout", 0)
     end
+    print(
+        "channel open, sending",
+        serialization.serialize({headers = headers, body = body})
+    )
     network.tcp.send(
         channel,
         serialization.serialize({headers = headers, body = body})
     )
+    print("waiting on", "message" .. request_id)
     event.pull(response_timeout, "message" .. request_id)
+    print("wait over", "response", response)
     if response == nil then
+        print("no response recieved")
         if channel_open then
+            print("channel still open, closing")
             network.tcp.close(channel)
         end
+        print("remove tcp handler")
         event.ignore("tcp", handler)
         error("response timeout", 0)
     end
     local status = 500
     local response_headers = {}
     local response_body = nil
+    print("parsing response")
     if type(response) == "table" then
+        print("response object found")
         if type(response.status) == "number" then
             status = response.status
+            print("found status", status)
         end
         if type(response.headers) == "table" then
             response_headers = response.headers
+            print("found headers", headers)
         end
-        if type(response.body) == "table" then
+        if type(response.body) == "string" then
             response_body = response.body
+            print("found body", body)
         end
     end
+    print(
+        "status",
+        status,
+        "headers",
+        headers,
+        "body",
+        body
+    )
     if channel_open then
+        print("channel open, closing")
         network.tcp.close(channel)
     end
+    print("end request")
     event.ignore("tcp", handler)
+    print("return request")
     return status, response_headers, response_body
 end
 return ____exports
--- src/lttp/lib/lttp.ts
+++ src/lttp/lib/lttp.ts
@@ -24,16 +24,21 @@ const tcp_listeners: {
 } = {};
 
 function getPort(args: TcpEventArgs) {
+    print("getPort", ...args)
     if(args[0] == "connection" || args[0] == "close") {
+        print("getPort => args[3] =", args[3])
         return args[3];
     }
     else if(args[0] == "message") {
+        print("getPort => args[4] =", args[4])
         return args[4];
     }
+    print("getPort => -1; unknown type", args[0])
     return -1;
 }
 
 export function listen(port: Port, timeout: number = 5): void {
+    print("listen", port, timeout);
     if(tcp_listeners[port] != undefined) {
         throw "Port already bound";
     }
@@ -41,24 +46,37 @@ export function listen(port: Port, timeout: number = 5): void {
     const message_timeouts: { [channel: Channel]: number | null } = {};
 
     tcp_listeners[port] = (name, ...args) => {
+        print("tcp_listener[", port, "]", name, ...args);
         if(getPort(args) === port) {
+            print("port match")
             const [action, channel] = args;
+            print("action", action, "channel", channel)
 
             if(action == "connection") {
-                const timeout_id= event.timer(timeout, () => network.tcp.close(channel));
+                print("handle connection");
+                const timeout_id= event.timer(timeout, () => {
+                    print("timed out, closing", channel);
+                    network.tcp.close(channel)
+                });
                 message_timeouts[channel] = timeout_id;
             }
             else if(action == "close") {
+                print("handle close")
                 if(message_timeouts[channel] != null) {
+                    print("timeout still active, cancelling");
                     event.cancel(message_timeouts[channel]);
                     message_timeouts[channel] = null;
                 }
             }
             else if(action == "message") {
+                print("handle message")
                 const document = args[2];
                 const address = args[3];
+                print("document", document);
+                print("address", address);
 
                 const respond = (status: number, headers?: Headers, body?: Body) => {
+                    print("respond", channel, status, headers, body);
                     network.tcp.send(
                         channel,
                         serialization.serialize({
@@ -73,21 +91,28 @@ export function listen(port: Port, timeout: number = 5): void {
                 let body: Body = null;
 
                 try {
+                    print("trying parse")
                     const document_data = serialization.unserialize(document);
+                    print("document_data", document_data);
                     if(typeof document_data === "object") {
+                        print("document is object")
                         if(typeof document_data.headers === "object") {
+                            print("document headers")
                             headers = document_data.headers;
                         }
                         if(typeof document_data.body === "string") {
+                            print("document body")
                             body = document_data.body;
                         }
                     }
                 }
                 catch {
+                    print("invalid")
                     respond(422);
                     return;
                 }
 
+                print("push lttp_request")
                 computer.pushSignal(
                     "lttp_request",
                     channel,
@@ -100,12 +125,15 @@ export function listen(port: Port, timeout: number = 5): void {
             }
         }
     }
+    print("adding listener to port", port);
     event.listen("tcp", tcp_listeners[port]);
     network.tcp.listen(port);
 }
 
 export function unlisten(port: Port): void {
+    print("ending listener on port", port)
     if(tcp_listeners[port] != null) {
+        print("removing event listener")
         event.ignore("tcp", tcp_listeners[port]);
         tcp_listeners[port] = null;
     }
@@ -124,48 +152,75 @@ export function request(
     Headers,
     Body,
 ]> {
+    print("request", address, port, headers, body, connection_timeout, response_timeout);
     const request_id = uuid.next();
+    print("request_id", request_id)
     let channel: number | null = null;
 
     let channel_open = false;
     let response: string | null = null;
 
     const handler = (name: string, ...args: [...TcpEventArgs]) => {
+        print("client handler", name, ...args);
         const [action, actual_channel] = args;
+        print("action", action, "actual_channel", actual_channel, "channel", channel)
         if(channel == actual_channel) {
+            print("channel match")
             if(action == "connection") {
+                print("handle connection")
                 channel_open = true;
+                print("push", "connect" + request_id);
                 computer.pushSignal("connect" + request_id);
             }
             if(action == "close") {
+                print("handle close")
                 channel_open = false;
                 // stop waiting
+                print("push", "connect" + request_id)
                 computer.pushSignal("connect" + request_id);
+                print("push", "message" + request_id)
                 computer.pushSignal("message" + request_id);
             }
             if(action == "message") {
+                print("handle message");
                 response = args[2];
+                print("response", response);
+                print("push", "message" + request_id)
                 computer.pushSignal("message" + request_id);
             }
         }
     }
 
+    print("listen to tcp events")
     event.listen("tcp", handler);
     channel = network.tcp.open(address, port);
+    print("tcp connection requested on channel", channel)
+    print("waiting on", "connect" + request_id)
     event.pull(connection_timeout, "connect" + request_id);
+    print("wait over", "channel_open", channel_open)
     if(!channel_open) {
+        print("channel never opened");
         event.ignore("tcp", handler);
         throw "connection timeout";
     }
+    print("channel open, sending", serialization.serialize({
+        headers,
+        body,
+    }));
     network.tcp.send(channel, serialization.serialize({
         headers,
         body,
     }));
+    print("waiting on", "message" + request_id)
     event.pull(response_timeout, "message" + request_id);
+    print("wait over", "response", response);
     if(response == null) {
+        print("no response recieved")
         if(channel_open) {
+            print("channel still open, closing")
             network.tcp.close(channel);
         }
+        print("remove tcp handler");
         event.ignore("tcp", handler);
         throw "response timeout";
     }
@@ -174,23 +229,32 @@ export function request(
     let response_headers: Headers = {};
     let response_body: Body = null;
 
+    print("parsing response")
     if(typeof response == "object") {
+        print("response object found")
         type ItsNotNever = { [key: string]: unknown };
         if(typeof (response as ItsNotNever).status === "number") {
             status = (response as ItsNotNever).status as number;
+            print("found status", status)
         }
         if(typeof (response as ItsNotNever).headers === "object") {
             response_headers = (response as ItsNotNever).headers as Headers;
+            print("found headers", headers);
         }
-        if(typeof (response as ItsNotNever).body === "object") {
+        if(typeof (response as ItsNotNever).body === "string") {
             response_body = (response as ItsNotNever).body as Body;
+            print("found body", body)
         }
     }
+    print("status", status, "headers", headers, "body", body)
 
     if(channel_open) {
+        print("channel open, closing");
         network.tcp.close(channel);
     }
+    print("end request")
     event.ignore("tcp", handler);
 
+    print("return request");
     return $multi(status, response_headers, response_body);
 }
